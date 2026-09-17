4c2c98654cd84196383cb1530ac64c49dd7bd6f0
yassincode20 <xistyxistyde@gmail.com>
2026-09-17 22:26:56 +0300

    Small changes to token expiraation duration

--- my-chef.py
+++ my-chef.py
@@ -16,11 +16,11 @@ import chef
 load_dotenv()
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = os.getenv("JWT_ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_DAYS = int(os.getenv("ACCESS_TOKEN_EXPIRE_DAYS"))
 
 
 def create_access_token(user_id: int):
-    expire = datetime.now(timezone.utc) + timedelta(days=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
 
     payload = {"sub": str(user_id), "exp": expire}
 
