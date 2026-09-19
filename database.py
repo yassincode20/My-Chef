f27632f5479870a2fa1923f3a07ac07c17214659
yassincode20 <xistyxistyde@gmail.com>
2026-09-19 13:07:25 +0300

    hidden db password

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-url = "postgresql://postgres:2009@localhost:5432/mychef"
+url = "postgresql://postgres:password@localhost:5432/mychef"
 engine = create_engine(url)
 session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
